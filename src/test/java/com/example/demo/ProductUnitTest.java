0c67d8cf06f5e51fd0c3d9cdd6c0b9d36999663e
akiaksdon <akshay.d.waghmare@gmail.com>
2022-08-11 09:35:42 +0000

    New Junit for hashmap

---
+++ src/test/java/com/example/demo/ProductUnitTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.HashMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ProductUnitTests {
+
+	@Test
+	public void getExistingValue() {
+		HashMap<String,Product> producyByname = new HashMap<>();
+
+		Product lorealShampoo = new Product("Loreal shampoo" , "Loreal brand");
+		Product ogxShampoo = new Product("OGX shampoo" , "OGX brand");
+
+		producyByname.put(lorealShampoo.getName(), lorealShampoo);
+		producyByname.put(ogxShampoo.getName(), ogxShampoo);
+
+		Product productPurchase = producyByname.get(lorealShampoo.getName());
+
+		assertArrayEquals("Loreal brand".toCharArray(), productPurchase.getDescription().toCharArray());
+	}
+
+}
